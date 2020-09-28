982ba4270a6404f8ebdbcaf7228e0bb2c5830f07
lekegitrepo <lekedevrepo@gmail.com>
2020-09-28 10:11:14 -0500

    add another solution for the single_number problem

--- single_number.rb
+++ single_number.rb
@@ -13,6 +13,16 @@ def single_number(nums)
   0
 end
 
+# def single_number(nums)
+#   set = Set.new
+
+#   nums.each do |num|
+#     set.include?(num) ? set.delete(num) : set << num
+#   end
+
+#   set.to_a.first
+# end
+
 p single_number([2, 2, 1, 1])
 
 p single_number([4, 1, 2, 1, 2])
