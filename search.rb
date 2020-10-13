e4dcdbc151d54a49a58dff4f01a88bf00816ee54
lekegitrepo <lekedevrepo@gmail.com>
2020-10-13 02:24:56 -0500

    Search in Rotated Sorted Array

---
+++ search.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def search(nums, target)
+  nums.index(target).nil? ? -1 : nums.index(target)
+end
+
+p search([4, 5, 6, 7, 0, 1, 2], 0)
+
+p search([4, 5, 6, 7, 0, 1, 2], 3)
+
+p search([1], 0)
